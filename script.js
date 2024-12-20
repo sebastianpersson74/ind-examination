980c8652f5c1b865074ae7ef0654e248896254f1
sebastianpersson74 <sebastianpersson74@gmail.com>
2024-12-20 22:12:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+START
+
+FUNCTION SplitNota
+    INPUT "Summa:" 
+    SET totalSum = INPUT
+
+    INPUT "Antal vänner:"
+    SET numberOfFriends = INPUT
+
+    INPUT "Dricks:"
+    SET tipPercentage = INPUT
+
+    CALCULATE tipAmount = totalSum * tipPercentage
+    CALCULATE totalWithTip = totalSum + tipAmount
+
+    CALCULATE amountPerPerson = totalWithTip / numberOfFriends
+
+    PRINT "Totalkostnaden inklusive dricks är:" totalWithTip
+    PRINT "Varje vän ska betala:" amountPerPerson
+ENDFUNCTION
+
+CALL FUNCTION SplitNota
+
+END
+
+*/ Länk till flödesschema - https://ibb.co/28yBSBq */
+
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
